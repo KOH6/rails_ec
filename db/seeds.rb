98235149ca1d3f86aecb82a31c8b8918151c96cf
KOH6 <minagk38+engineer@gmail.com>
2023-09-05 20:49:12 +0900

    動的な商品詳細画面作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 require 'aws-sdk-s3'
 
 # 本番環境の場合S3にテストデータ用画像が保存されているため、Herokuのローカルへ再保存する。
+image_path = 'app/assets/images/dummy.jpg'
+
 if Rails.env.production?
   s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
   obj = s3.bucket(ENV['AWS_BUCKET']).object('images/450x300.jpg')
-  obj.get(response_target: 'app/assets/images/450x300.jpg')
+  obj.get(response_target: image_path)
 end
 
 10.times do |n|
@@ -18,7 +20,7 @@ end
     sku: "testCode:#{count}",
     price: 1000 * count,
     stock: 5 * count,
-    description: "descriptionTest#{count}"
+    description: "descriptionTest#{count}詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト詳細テスト"
   )
-  product.image.attach(io: File.open(Rails.root.join('app/assets/images/450x300.jpg')), filename: 'dummy.jpg')
+  product.image.attach(io: File.open(Rails.root.join(image_path)), filename: 'dummy.jpg')
 end
