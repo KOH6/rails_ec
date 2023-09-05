6cad3d4bc9e9a667075eeb2c37ad20c49344ee9b
KOH6 <minagk38+engineer@gmail.com>
2023-09-05 17:24:25 +0900

    seedの本番環境用処理を微調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 require 'aws-sdk-s3'
+
+# 本番環境の場合S3にテストデータ用画像が保存されているため、Herokuのローカルへ再保存する。
+if Rails.env == 'production'
+  s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
+  obj = s3.bucket(ENV['AWS_BUCKET']).object('images/450x300.jpg')
+  obj.get(response_target: 'app/assets/images/450x300.jpg')
+end
 
 10.times do |n|
   count = n + 1
@@ -18,10 +20,5 @@ require 'aws-sdk-s3'
     star: count % 6,
     description: "descriptionTest#{count}"
   )
-  if Rails.env == 'production'
-    s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
-    obj = s3.bucket(ENV['AWS_BUCKET']).object('images/450x300.jpg')
-    obj.get(response_target: 'app/assets/images/450x300.jpg')
-  end
   product.image.attach(io: File.open(Rails.root.join('app/assets/images/450x300.jpg')), filename: 'dummy.jpg')
 end
