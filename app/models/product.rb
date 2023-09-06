385ea4a1deaea10967024e5bb230a6260600b9e7
KOH6 <minagk38+engineer@gmail.com>
2023-09-06 20:52:04 +0900

    rails_best_practices対応default_scope削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-  # 論理削除対応。デフォルトのスコープを論理削除されていないものを返すように指定する。
   include Discard::Model
-  default_scope -> { kept }
 
   has_one_attached :image
 end
