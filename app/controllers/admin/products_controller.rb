aa41b243562c96c8ef23716d2ab9a88551261ddf
KOH6 <minagk38+engineer@gmail.com>
2023-09-07 16:38:21 +0900

    商品削除機能実装

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -10,4 +10,14 @@ class Admin::ProductsController < ApplicationController
 
   def new
   end
+
+  def destroy
+    product = Product.find(params[:id])
+    if product.discard
+      flash[:success] = "#{product.name}の削除が完了しました"
+    else
+      flash[:danger] = "#{product.name}の削除に失敗しました"
+    end
+    redirect_to admin_products_path
+  end
 end
