c68d74265c719b634d1a18f406a78fb71f03f101
KOH6 <minagk38+engineer@gmail.com>
2023-09-09 00:41:08 +0900

    rails_best_practices対応partialの変数、law of demeter

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -3,6 +3,7 @@
 class CartProduct < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+  delegate :name, :price, :to => :product, :prefix => true
 
   def subtotal
     product.price * quantity
