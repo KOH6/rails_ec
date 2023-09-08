cd58d44eafdeec350779768e89a9bf7ec26c0718
KOH6 <minagk38+engineer@gmail.com>
2023-09-08 18:46:37 +0900

    サイト訪問時セッションにカートid格納、カート画面への繊維

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_cart_id
+
+  def set_cart_id
+    session[:cart_id] ||= Cart.create.id
+    logger.debug("session")
+    logger.debug(session[:cart_id])
+  end
 end
