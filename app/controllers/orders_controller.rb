6729dc6cc724e2eb5ed9ffa8dd0ba5ca3c53e3d9
KOH6 <minagk38+engineer@gmail.com>
2023-09-10 18:36:25 +0900

    チェックアウト処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,10 +6,15 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
+  # Orderレコード作成時に、カートの削除、購入明細の作成を行う
   def create
+    # TODO:在庫照合バリデーション
     @order = Order.new(order_params)
     if @order.save
       flash[:success] = "購入ありがとうございます"
+      create_order_products(cart_products: @cart_products, order_id: @order.id)
+      destroy_cart(cart_id: @cart_id)
+      session[:cart_id] = nil
       redirect_to products_path
     else
       render :index
@@ -29,5 +34,17 @@ class OrdersController < ApplicationController
   def set_cart_products
     @cart_products = Cart.find(@cart_id).cart_products.order(created_at: :desc)
     @total = @cart_products.inject(0) { |total, cart_product| total + cart_product.subtotal }
+  end
+
+  def create_order_products(cart_products:, order_id:)
+    cart_products.each do |cart_product|
+      product_id = cart_product.product_id
+      quantity = cart_product.quantity
+      OrderProduct.create(order_id:, product_id:, quantity:)
+    end
+  end
+
+  def destroy_cart(cart_id:)
+    Cart.find(cart_id).destroy
   end
 end
