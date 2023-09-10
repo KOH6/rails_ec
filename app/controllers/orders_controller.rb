2a857665a68d83f390407f952e9cad57caa43369
KOH6 <minagk38+engineer@gmail.com>
2023-09-10 21:49:01 +0900

    Revert "OrderFormに処理を移行"
    This reverts commit 9b7d38065bb25f914175fc0eec511fe1af8478e6.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,16 +3,16 @@ class OrdersController < ApplicationController
   before_action :set_cart_products, only: %i[index create]
 
   def index
-    @order = OrderForm.new
+    @order = Order.new
   end
 
   def create
     # TODO:在庫照合バリデーション
-    @order = OrderForm.new(order_params)
-    if @order.valid?
+    @order = Order.new(order_params)
+    if @order.save
       # Orderレコード作成時に、カートの削除、購入明細の作成、セッションの削除を行う
-      @order.save(cart_products: @cart_products, cart_id: @cart_id)
-
+      create_order_products(cart_products: @cart_products, order_id: @order.id)
+      Cart.find(@cart_id).destroy
       session[:cart_id] = nil
 
       flash[:success] = "購入ありがとうございます"
@@ -29,11 +29,19 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_form).permit(%i[last_name first_name user_name email country prefecture zip_code address1 address2 credit_name credit_number credit_expiration credit_cvv])
+    params.require(:order).permit(%i[last_name first_name user_name email country prefecture zip_code address1 address2 credit_name credit_number credit_expiration credit_cvv])
   end
 
   def set_cart_products
     @cart_products = Cart.find(@cart_id).cart_products.order(created_at: :desc)
     @total = @cart_products.inject(0) { |total, cart_product| total + cart_product.subtotal }
+  end
+
+  def create_order_products(cart_products:, order_id:)
+    cart_products.each do |cart_product|
+      product_id = cart_product.product_id
+      quantity = cart_product.quantity
+      OrderProduct.create(order_id:, product_id:, quantity:)
+    end
   end
 end
