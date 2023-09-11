42c34af2f345eb949c70e0319204141ea38f470b
KOH6 <minagk38+engineer@gmail.com>
2023-09-11 21:30:39 +0900

    商品一覧を作成日降順に変更

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,7 +5,7 @@ module Admin
     before_action :set_product, only: %i[show update destroy]
 
     def index
-      @products = Product.kept.order(updated_at: :desc)
+      @products = Product.kept.order(created_at: :desc)
     end
 
     def show; end
